334a95e5e324469fa9330db1f403d4ca3154c2eb
BiancaTopan <bianca.corina65@yahoo.com>
2020-04-27 15:02:59 +0300

    Create create-request.js

---
+++ game2/client/create-request.js
@@ -0,0 +1,11 @@
+function createRequest(method, url, data, callback=console.log) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.onload = function () {
+        var responseText = xhr.responseText;
+        // console.log(responseText);
+         callback(responseText);  
+    };
+    xhr.send(data);
+}
+
